3e8516cd005d4416d55a17066fb443822f1aaebb
Nazarii Zastavnyi <nazar.zastavniy@dashdevs.com>
2025-11-05 14:50:40 +0100

    FCLIQU-1104: fix close problem updated Chronicle

--- src/main/java/exchange/core2/core/processors/journaling/DiskSerializationProcessor.java
+++ src/main/java/exchange/core2/core/processors/journaling/DiskSerializationProcessor.java
@@ -232,7 +232,7 @@ public final class DiskSerializationProcessor implements ISerializationProcessor
 
         @Override
         public void close() {
-            bytes.release();
+            bytes.releaseLast();
         }
     }
 
