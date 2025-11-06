e231fae2fd52c3906c196b27910beba3a41e7bf3
Nazarii Zastavnyi <nazar.zastavniy@dashdevs.com>
2025-11-06 20:51:25 +0100

    FCLIQU-1104: fixes for snapshoting

--- src/main/java/exchange/core2/core/processors/journaling/DiskSerializationProcessor.java
+++ src/main/java/exchange/core2/core/processors/journaling/DiskSerializationProcessor.java
@@ -46,6 +46,7 @@ import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
+import java.nio.Buffer;
 
 
 @Slf4j
@@ -231,7 +232,7 @@ public final class DiskSerializationProcessor implements ISerializationProcessor
 
         @Override
         public void close() {
-            bytes.release();
+            bytes.releaseLast();
         }
     }
 
@@ -659,30 +660,30 @@ public final class DiskSerializationProcessor implements ISerializationProcessor
         if (journalWriteBuffer.position() < journalBatchCompressThreshold) {
             // uncompressed write for single messages or small batches
             writtenBytes += journalWriteBuffer.position();
-            journalWriteBuffer.flip();
+            ((Buffer) journalWriteBuffer).flip();
 //            long t = System.nanoTime();
             channel.write(journalWriteBuffer);
 //            hdrRecorderRaw.recordValue(System.nanoTime() - t);
-            journalWriteBuffer.clear();
+            ((Buffer) journalWriteBuffer).clear();
 
         } else {
             // compressed write for bigger batches
 //            long t = System.nanoTime();
             int originalLength = journalWriteBuffer.position(); // commands code
-            journalWriteBuffer.flip();
+            ((Buffer) journalWriteBuffer).flip();
             lz4WriteBuffer.put(OrderCommandType.RESERVED_COMPRESSED.getCode()); // compressed block
             lz4WriteBuffer.putInt(0); // reserve space
             lz4WriteBuffer.putInt(0); // reserve space
             lz4CompressorJournal.compress(journalWriteBuffer, lz4WriteBuffer);
-            journalWriteBuffer.clear();
+            ((Buffer) journalWriteBuffer).clear();
             writtenBytes += lz4WriteBuffer.position();
             int remainingCompressedLength = lz4WriteBuffer.position() - 9; // 1 + 4 + 4
             lz4WriteBuffer.putInt(1, remainingCompressedLength); // 1 byte offset
             lz4WriteBuffer.putInt(5, originalLength); // 1 + 4 bytes offset
-            lz4WriteBuffer.flip();
+            ((Buffer) lz4WriteBuffer).flip();
 //            hdrRecorderLz4.recordValue(System.nanoTime() - t);
             channel.write(lz4WriteBuffer);
-            lz4WriteBuffer.clear();
+            ((Buffer) lz4WriteBuffer).clear();
         }
 
         if (forceStartNextFile || writtenBytes >= journalFileMaxSize) {
