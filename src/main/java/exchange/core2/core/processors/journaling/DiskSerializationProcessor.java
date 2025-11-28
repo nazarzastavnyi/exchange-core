7c8e1b214671f7eedd191db9b0fc5d1cdbefd869
Nazarii Zastavnyi <nazar.zastavniy@dashdevs.com>
2025-11-28 19:16:22 +0100

    FCLIQU-1104 just send every time events

--- src/main/java/exchange/core2/core/processors/journaling/DiskSerializationProcessor.java
+++ src/main/java/exchange/core2/core/processors/journaling/DiskSerializationProcessor.java
@@ -421,7 +421,7 @@ public final class DiskSerializationProcessor implements ISerializationProcessor
 
 //        log.info("Read total: {} bytes ", totalBytesRead);
 
-        // api.groupingControl(0, 0);
+        api.groupingControl(0, 0);
 
 
 
